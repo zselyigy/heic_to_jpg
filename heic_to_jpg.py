deb6c7ee84e576898b9d7c6d731fceb3441bc56b
István Gy. Zsély <zselyigy@gmail.com>
2024-01-31 19:56:58 +0100

    Created final GUI

--- heic_to_jpg.py
+++ heic_to_jpg.py
@@ -8,8 +8,13 @@ import customtkinter as ctk
 from tkinter import filedialog
 
 def gui():
-    global label
+    global label, quall
     global recc, delc, zipc
+    global modesw
+    global quals
+    global q
+    q = 95
+    
     #Set the theme and color options
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("dark-blue")
@@ -20,21 +25,66 @@ def gui():
     root.title("HEIC to jpg converter")
     #Set GUI size to zoomed
     root.after(1, lambda: root.state("zoomed"))
-    
-    fb = ctk.CTkButton(master=root, text="Choose file", command=get_file).pack()
-    db = ctk.CTkButton(master=root, text="Choose directory", command=get_directory).pack()
-    fsb = ctk.CTkButton(master=root, text="Choose files", command=get_files).pack()
-    convfb = ctk.CTkButton(master=root, text="Convert files/directory", command=conversion).pack()
-    recc = ctk.CTkCheckBox(master=root, text="Run in all subdirectories?", onvalue=True, offvalue=False)
-    delc = ctk.CTkCheckBox(master=root, text="Delete processed HEIC files?", onvalue=True, offvalue=False)
-    zipc = ctk.CTkCheckBox(master=root, text="Zip processed HEIC files?", onvalue=True, offvalue=False)
-    recc.pack()
-    delc.pack()
-    zipc.pack()
-    label = ctk.CTkLabel(root, text="File or directory path will appear here.")
-    label.pack()
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=1)
+    root.columnconfigure(2, weight=1)
+    root.columnconfigure(3, weight=1)
+    root.columnconfigure(4, weight=1)
+    root.columnconfigure(5, weight=1)
+    root.columnconfigure(6, weight=1)
+    root.columnconfigure(7, weight=1)
+    root.columnconfigure(8, weight=1)
+    root.columnconfigure(9, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=1)
+    root.rowconfigure(3, weight=1)
+    root.rowconfigure(4, weight=1)
+    root.rowconfigure(5, weight=1)
+    root.rowconfigure(6, weight=1)
+    root.rowconfigure(7, weight=1)
+    root.rowconfigure(8, weight=1)
+    root.rowconfigure(9, weight=1)
+
+    fb = ctk.CTkButton(master=root, text="Choose file", command=get_file, height=100, font=("Arial", 20))
+    db = ctk.CTkButton(master=root, text="Choose directory", command=get_directory, height=100, font=("Arial", 20))
+    fsb = ctk.CTkButton(master=root, text="Choose files", command=get_files, height=100, font=("Arial", 20))
+    convfb = ctk.CTkButton(master=root, text="Convert files/directory", command=conversion, height=100, font=("Arial", 20))
+    recc = ctk.CTkCheckBox(master=root, text="Run in all subdirectories?", onvalue=True, offvalue=False,font=("Arial", 20))
+    delc = ctk.CTkCheckBox(master=root, text="Delete processed HEIC files?", onvalue=True, offvalue=False,font=("Arial", 20))
+    zipc = ctk.CTkCheckBox(master=root, text="Zip processed HEIC files?", onvalue=True, offvalue=False,font=("Arial", 20))
+    modesw = ctk.CTkSwitch(master=root, text="Dark mode", onvalue=True, offvalue=False, command=change_mode,font=("Arial", 20))
+    quals = ctk.CTkSlider(master=root, from_=1, to=100, command=quality, progress_color="#1f538d", number_of_steps=99, button_color="#102a47")
+    quals.grid(row=3, column=4, sticky="we")
+    modesw.grid(row=9, column=0, sticky="we")
+    recc.grid(row=4, column=4, sticky="we")
+    delc.grid(row=5, column=4, sticky="we")
+    zipc.grid(row=6, column=4, sticky="we")
+    fb.grid(row=1, column=0, sticky="we")
+    fsb.grid(row=1, column=2, sticky="we")
+    db.grid(row=1, column=6, sticky="we")
+    convfb.grid(row=1, column=4, sticky="we")
+    modesw.select()
+    quals.set(95)
+    label = ctk.CTkLabel(root, width=root.winfo_width(), text="File or directory path will appear here.", corner_radius=5, anchor="center", font=("Arial", 20))
+    quall = ctk.CTkLabel(root, text=f"Quality: {q}", corner_radius=5, font=("Arial", 20))
+    quall.grid(row=2, column=4, sticky="we")
+    label.grid(row=0, column=4, sticky= "we")
     root.mainloop()
     
+def change_mode():
+    if modesw.get():
+        ctk.set_appearance_mode("dark")
+        quals.configure(progress_color="#1f538d", button_color="#102a47")
+    else:
+        ctk.set_appearance_mode("light")
+        quals.configure(progress_color="#4c75a4", button_color = "#1f538d")
+
+def quality(new):
+    global q
+    q = int(new)
+    quall.configure(text=f"Quality: {q}")
+    print(q)
 
 def folders():
     i = 0
@@ -53,7 +103,7 @@ def convert_rec(directorylist):
         files = [f for f in os.listdir(directory) if f.lower().endswith('.heic') or f.lower().endswith('.heif')]
         if len(files) != 0:
             for filename in files:
-                command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{directory}" -q 95 "{directory}/{filename}"'
+                command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{directory}" -q {q} "{directory}/{filename}"'
                 #
                 try:
                     subprocess.run(command, shell=True, check=True)
@@ -87,7 +137,7 @@ def convert(directory):
     files = [f for f in os.listdir(directory) if f.lower().endswith('.heic') or f.lower().endswith('.heif')]
     if len(files) != 0:
         for filename in files:
-            command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{directory}" -q 95 "{directory}/{filename}"'
+            command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{directory}" -q {q} "{directory}/{filename}"'
             #
             try:
                 subprocess.run(command, shell=True, check=True)
@@ -117,7 +167,7 @@ def convert(directory):
                 for file_to_write in files:
                     os.remove(f"{directory}/{file_to_write}")
 def convert_f(file):
-    command = rf'.\.venv\Scripts\heif-convert.exe -o {file[:-5]} -p "{file}" -q 95 "{file}"'
+    command = rf'.\.venv\Scripts\heif-convert.exe -o {file[:-5]} -p "{file}" -q {q} "{file}"'
     #
     try:
         subprocess.run(command, shell=True, check=True)
@@ -129,7 +179,7 @@ def convert_f(file):
 
 def convert_fs(filelist):
     for filename in filelist:
-        command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{os.path.dirname(filename)}" -q 95 "{filename}"'
+        command = rf'.\.venv\Scripts\heif-convert.exe -o {filename[:-5]} -p "{os.path.dirname(filename)}" -q {q} "{filename}"'
         try:
             subprocess.run(command, shell=True, check=True)
             logging.info(f"{os.path.basename(filename)} ---> {os.path.basename(filename)}.jpg")
@@ -177,8 +227,9 @@ def get_file():
     global dirs
     dirs = ""
     file = filedialog.askopenfile(title="Select a HEIC file", filetypes=[("HEIC file","*.HEIC")])
-    label.configure(text=f"Selected file: {file.name}")
-    label.pack()
+    if file != "":
+        label.configure(text=f"Selected file: {file.name}")
+        label.grid(row=0, column=4, sticky= "we")
     
 
 def get_directory():
@@ -186,16 +237,18 @@ def get_directory():
     global file
     file = ""
     dirs = filedialog.askdirectory(title="Select directory")
-    label.configure(text=f"Selected directory: {dirs}")
-    label.pack()
+    if dirs != "":
+        label.configure(text=f"Selected directory: {dirs}")
+        label.grid(row=0, column=4, sticky= "we")
     
 def get_files():
     global file
     global dirs
     dirs = ""
-    file = filedialog.askopenfiles(title="Select a HEIC file", filetypes=[("HEIC file","*.HEIC")])
-    label.configure(text=f"Selected files: {[f.name for f in file]}")
-    label.pack()
+    file = filedialog.askopenfiles(title="Select HEIC files", filetypes=[("HEIC file","*.HEIC")])
+    if len(file) != 0:
+        label.configure(text=f"Selected files: {[f.name for f in file]}")
+        label.grid(row=0, column=4, sticky= "we")
     
 def main():
     logging.basicConfig(filename="latest.log", level=logging.INFO, filemode='w',format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
